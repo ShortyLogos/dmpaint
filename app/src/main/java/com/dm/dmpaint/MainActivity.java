b103c4c05863e8871d59eb948aee6af813683ae5
Déric Marchand <dericmarchand@gmail.com>
2022-02-15 20:30:42 -0500

    working pipette

--- app/src/main/java/com/dm/dmpaint/MainActivity.java
+++ app/src/main/java/com/dm/dmpaint/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout colorsPalette;
     LinearLayout tools;
     ImageView undo;
+    ImageView pipette;
+    ImageView cercle;
+    ImageView carre;
     ImageView supprimer;
 
     Vector<Dessin> objetsDessin = new Vector<Dessin>();
@@ -47,10 +51,13 @@ public class MainActivity extends AppCompatActivity {
     Path traceLibre;
 
     Paint crayonPlein, crayonContour;
+    Point depart;
+    Point arrivee;
 
     int couleurActive = Color.BLACK;
     Integer couleurFondActive = Color.WHITE;
     int largeurActive;
+    String outilActif = "traceLibre";
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -65,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
         displayActiveColor = findViewById(R.id.displayActiveColor);
         tools = findViewById(R.id.tools);
         undo = findViewById(R.id.undo);
+        pipette = findViewById(R.id.pipette);
+        cercle = findViewById(R.id.cercle);
+        carre = findViewById(R.id.carre);
         supprimer = findViewById(R.id.supprimer);
 
         // 1. Mise en place de la surface de dessin
@@ -100,9 +110,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+//        for (int i = 0; i < tools.getChildCount(); i++) {
+//            if (colorsPalette.getChildAt(i) instanceof ImageView) {
+//                colorsPalette.getChildAt(i).setOnClickListener(ecOutils);
+//            }
+//        }
+
         surface.setOnTouchListener(ecSurface);
         largeur.setOnSeekBarChangeListener(ecOutils);
         undo.setOnClickListener(ecOutils);
+        pipette.setOnClickListener(ecOutils);
+        cercle.setOnClickListener(ecOutils);
+        carre.setOnClickListener(ecOutils);
         supprimer.setOnClickListener(ecOutils);
     }
 
@@ -121,18 +140,55 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onTouch(View source, MotionEvent motionEvent) {
+
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                traceLibre = new Path();
-                traceLibre.moveTo(motionEvent.getX(), motionEvent.getY());
-                surface.invalidate();
+
+                if (outilActif.equals("traceLibre")) {
+                    traceLibre = new Path();
+                    traceLibre.moveTo(motionEvent.getX(), motionEvent.getY());
+                    surface.invalidate();
+                }
+                else if (outilActif.equals("pipette")) {
+                    outilActif = "pipette";
+                    Bitmap bitmap = surface.getBitmapImage();
+                    couleurActive = bitmap.getPixel((int)motionEvent.getX(), (int)motionEvent.getY());
+                }
+                else if (outilActif.equals("cercle") || outilActif.equals("carre")) {
+                    depart = new Point((int)motionEvent.getX(), (int)motionEvent.getY());
+                    surface.invalidate();
+                }
+
             }
             else if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-                traceLibre.lineTo(motionEvent.getX(), motionEvent.getY());
-                surface.invalidate();
+
+                if (outilActif.equals("traceLibre")) {
+                    traceLibre.lineTo(motionEvent.getX(), motionEvent.getY());
+                    surface.invalidate();
+                }
+                else if (outilActif.equals("pipette")) {
+                    outilActif = "pipette";
+                    Bitmap bitmap = surface.getBitmapImage();
+                    couleurActive = bitmap.getPixel((int)motionEvent.getX(), (int)motionEvent.getY());
+                }
+                else if (outilActif.equals("cercle") || outilActif.equals("carre")) {
+                    arrivee = new Point((int)motionEvent.getX(), (int)motionEvent.getY());
+                    surface.invalidate();
+                }
+
             }
             else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                TraceLibre trait = new TraceLibre(couleurActive, largeurActive, traceLibre);
-                objetsDessin.add(trait);
+                depart = null;
+                arrivee = null;
+
+                if (outilActif.equals("traceLibre")) {
+                    TraceLibre trait = new TraceLibre(couleurActive, largeurActive, traceLibre);
+                    objetsDessin.add(trait);
+                }
+                else if (outilActif.equals("pipette")) {
+                    displayActiveColor.setBackgroundColor(couleurActive);
+                    outilActif = "traceLibre";
+                }
+
             }
 
             return true;
@@ -144,9 +200,15 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View source) {
             if (source == undo && objetsDessin.size() > 0) {
-                objetsDessin.remove(objetsDessin.size() -1);
+                objetsDessin.remove(objetsDessin.size() - 1);
                 surface.invalidate();
                 traceLibre = null;
+            }
+            else if (source == pipette) {
+                outilActif = "pipette";
+            }
+            else if (source == carre) {
+                outilActif = "carre";
             }
             else if (source == supprimer && objetsDessin.size() > 0)  {
                 objetsDessin.clear();
@@ -180,10 +242,6 @@ public class MainActivity extends AppCompatActivity {
             crayonPlein = new Paint(Paint.ANTI_ALIAS_FLAG);
             crayonContour = new Paint(Paint.ANTI_ALIAS_FLAG);
             crayonContour.setStyle(Paint.Style.STROKE);
-
-            // Pour des tests :
-            crayonPlein.setStrokeWidth(30);
-            crayonPlein.setColor(Color.BLACK);
         }
 
         // Redéfinition de la méthode onDraw de la classe interne Surface
@@ -193,7 +251,9 @@ public class MainActivity extends AppCompatActivity {
 
             surface.setBackgroundColor(couleurFondActive);
             crayonContour.setColor(couleurActive);
+            crayonPlein.setColor(couleurActive);
             crayonContour.setStrokeWidth(largeurActive);
+            crayonPlein.setStrokeWidth(largeurActive);
 
             // Ici on dessinera
             if (objetsDessin.size() > 0) {
@@ -205,6 +265,29 @@ public class MainActivity extends AppCompatActivity {
             if (traceLibre != null) {
                 canvas.drawPath(traceLibre, crayonContour);
             }
+
+            if (depart != null && arrivee != null) {
+
+                if (outilActif.equals("cercle")) {
+                    int deltaX = Math.abs(arrivee.x - depart.x) * Math.abs(arrivee.x - depart.x);
+                    int deltaY = Math.abs(arrivee.y - depart.y) * Math.abs(arrivee.y - depart.y);
+                    float rayon = (float) Math.sqrt(deltaX + deltaY);
+                    canvas.drawCircle(depart.x, depart.y, rayon, crayonPlein);
+                }
+
+                else if (outilActif.equals("carre")) {
+                    canvas.drawRect(depart.x, depart.y, arrivee.x, arrivee.y, crayonPlein);
+                }
+
+            }
+        }
+
+        public Bitmap getBitmapImage() {
+            this.buildDrawingCache();
+            Bitmap bitmapImage = Bitmap.createBitmap(this.getDrawingCache());
+            this.destroyDrawingCache();
+
+            return bitmapImage;
         }
     }
 
